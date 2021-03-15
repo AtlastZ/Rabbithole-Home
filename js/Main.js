21549022de945cc2c234b40665d83c54882bc518
AtlastZ <neolight413666@gmail.com>
2021-03-15 21:58:48 +0700

    first commit

---
+++ js/Main.js
@@ -0,0 +1,39 @@
+
+ function myFunction(input){
+  if(input ==1){
+    console.log("errrrrrrrrrr1");
+    document.getElementById("canvas").style.backgroundImage = "url('asset/Web_1920_2.jpg')";
+    document.getElementById("canvas").style.height = "50vw";
+  }else if(input ==2){
+    console.log("errrrrrrrrrr2");
+    document.getElementById("canvas").style.backgroundImage = "url('asset/Web_1920_3.jpg')";
+    document.getElementById("canvas").style.height = "50vw";
+  }else if(input ==3){
+    console.log("errrrrrrrrrr3");
+    document.getElementById("canvas").style.backgroundImage = "url('asset/Web_1920_5.jpg')";
+    document.getElementById("canvas").style.height = "50vw";
+  }else if(input ==4){
+    console.log("errrrrrrrrrr4");
+    document.getElementById("canvas").style.backgroundImage = "url('asset/Web_1920_4.jpg')";
+    document.getElementById("canvas").style.height = "50vw";
+  }
+ }
+  
+ $(window).on("load",function(){
+  $(".loader-wrapper").fadeOut("slow");
+});
+
+$(document).ready(function(){
+  $(".btn #play-btn").on('click', function(){
+    $(this).hide();
+    $("#pause-btn").fadeIn();
+    $("#music")[0].play();
+  });
+  $(".btn #pause-btn").on('click', function(){
+    $(this).hide();
+    $("#play-btn").fadeIn();
+    $("#music")[0].pause();
+  });
+});
+var music = document.getElementById("music");
+music.volume = 0.2;
